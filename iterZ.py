8017a3f4dbdcaee6fc61984389482c2a42a750dc
HuJyunKai <contact@sivilization.com>
2020-04-15 22:01:17 +0800

    Update iterZ.py

--- iterZ.py
+++ iterZ.py
@@ -1,9 +1,19 @@
+def decorator_iterZ(func):
+    def _decorator_iterZ(self,*args, **kwargs):
+        ret = func(self.obj,*args, **kwargs)
+        return iterZ(ret) if hasattr(ret,"__iter__") else ret
+    return _decorator_iterZ
+
 class iterZ:
     def __init__(self,self_obj):
         self.obj = self_obj
         for d in dir(self_obj):
             if d not in ["__class__"]:
-                setattr(self,d, getattr(self_obj, d))
+                attrset = getattr(self_obj, d)
+                if callable(attrset):
+                    decorator_iterZ(setattr(self,d, attrset))
+                else:
+                    setattr(self,d, attrset)
     def __iter__(self):
         return self.obj.__iter__()
     def __next__(self):
@@ -16,8 +26,6 @@ class iterZ:
         return self.obj.__len__()
     def __getitem__(self, x):
         return self.obj.__getitem__(x)
-    def __add__(self,a):
-        return iterZ(self.obj.__add__(a.obj if type(a) == iterZ else a))
     def __eq__(self,a):
         return iterZ(self.obj.__eq__(a.obj if type(a) == iterZ else a))
     def __ge__(self,a):
@@ -28,7 +36,8 @@ class iterZ:
         return iterZ(self.obj.__le__(a.obj if type(a) == iterZ else a))
     def __lt__(self,a):
         return iterZ(self.obj.__lt__(a.obj if type(a) == iterZ else a))
-    
+    def __add__(self,a):
+        return iterZ(self.obj.__add__(a.obj if type(a) == iterZ else a))
     def map(self,func):
         return iterZ(map(func,self.obj))
     def filter(self,func):
